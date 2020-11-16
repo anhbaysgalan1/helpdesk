84ecc429b022c3cf5c95ffe953603ea4c5f8a290
Awaliyatul Hikmah <57889114+ahikmah@users.noreply.github.com>
2020-11-16 21:48:07 +0700

    tes

---
+++ mapping.py
@@ -0,0 +1,475 @@
+mapping ={
+    # SAYA
+    'gw'    : 'saya',
+    'gue'   : 'saya',
+    'gua'   : 'saya',
+    'aku'   : 'saya',
+    'aq'    : 'saya',
+    'aku'   : 'saya',
+    'sya'   : 'saya',
+    'sy'    : 'saya',
+    'ane'   : 'saya',
+    'w'   : 'saya',
+    'q'   : 'saya',
+    
+    # YANG
+    'yg'    : 'yang',
+    'yng'   : 'yang',
+    'xg'   : 'yang',
+    
+    # AKTIF
+    'aktip' : 'aktif',
+    'aktf' : 'aktif',
+    'aktife' : 'aktif',
+    'aktive' : 'aktif',
+    'aktiv' : 'aktif',
+    'aktiph' : 'aktif',
+    
+    # DAFTAR
+    'dftr'  : 'daftar',
+    'dftar'  : 'daftar',
+    'daftr'  : 'daftar',
+    'regis'  : 'daftar',
+    'regist'  : 'daftar',
+    'registrasi'  : 'daftar',
+    
+    
+    # TIDAK
+    'ga'    : 'tidak',
+    'gak'   : 'tidak',      
+    'gk'    : 'tidak',
+    'ngga'  : 'tidak',
+    'nggak' : 'tidak',
+    'g'     : 'tidak',
+    'gg'    : 'tidak',
+    'gc'    : 'tidak',
+    'gx'    : 'tidak',
+    'tdk'   : 'tidak',
+    'tak'   : 'tidak',
+    'todak' : 'tidak',
+    'nda'   : 'tidak',
+    'ndak'  : 'tidak',
+    'tida'  : 'tidak',
+    'ngak'  : 'tidak',
+    'kaga'  : 'tidak',
+    'kagak'  : 'tidak',
+    
+    
+    # TARIF
+    'trif'  : 'tarif',
+    'tarip'  : 'tarif',
+    
+    # KENAPA
+    'knp'   : 'kenapa',
+    'knapa' : 'kenapa',
+    'knpa'  : 'kenapa',
+    
+    # BAGAIMANA
+    'gmn'   : 'bagaimana',
+    'gmna'  : 'bagaimana',
+    'gmna'  : 'bagaimana',
+    'gimana' : 'bagaimana',
+    'bgmana' : 'bagaimana',
+    'bgmna' : 'bagaimana',
+    
+    # MENGGUNAKAN
+    'pake'  : 'menggunakan',
+    'pakai' : 'menggunakan',
+    'pke'   : 'menggunakan',
+    'pakek' : 'menggunakan',
+    
+    # MENGISI
+    'ngisi' : 'mengisi',
+    'ngsi'  : 'mengisi',
+    'mngisi' : 'mengisi',
+    'mngsi' : 'mengisi',
+    
+    # SEPERTI
+    'kyk'   : 'seperti',
+    'kyak'  : 'seperti',
+    'kek'   : 'seperti',
+    
+    # DIMANA
+    'dmana' : 'dimana',
+    'dmn'   : 'dimana',
+    'dimna' : 'dimana',
+    
+    # TOLONG
+    'tlg'   : 'tolong',
+    'tlng'  : 'tolong',
+    'tlong' : 'tolong',
+    'tolng' : 'tolong',
+    'plis' : 'tolong',
+    'please' : 'tolong',
+    'pliz' : 'tolong',
+    
+    
+    # GOJEK
+    'go pay': 'gopay',
+    'go-pay': 'gopay',
+    'pay later': 'paylater',
+    'pay-later': 'paylater',
+    'go food'   : 'gofood',
+    'go-food'   : 'gofood',
+    'gfood'     : 'gofood',  
+    'go box'    : 'gobox',
+    'go-box'    : 'gobox',
+    'gobok'    : 'gobox',
+    'go bok'    : 'gobox',
+    'go-bok'    : 'gobox',
+    'gosen' : 'gosend',
+    'go send'   : 'gosend',
+    'go-send'   : 'gosend',
+    'gpsen' : 'gosend',
+    'gosen' : 'gosend',
+    'go car'    : 'gocar',
+    'go-car'    : 'gocar',
+    'go ride'    : 'goride',
+    'go-ride'    : 'goride',
+    'go bizz'   : 'gobizz',
+    'go-bizz'   : 'gobizz',
+    'go bill' : 'gobill',
+    'go-bill' : 'gobill',
+    'go bills' : 'gobill',
+    'go-bills' : 'gobill',
+    'pay leter' : 'paylater',
+    'pay-leter' : 'paylater',
+    'pay-letter' : 'paylater',
+    'pay letter' : 'paylater',
+    'payletter' : 'paylater',
+    
+      
+    # FOTO
+    'poto' : 'foto',
+    'photo' : 'foto',
+    'pto' : 'foto',
+    'fto' : 'foto',
+    
+    # SEKALI
+    'bgt'   : 'sekali',
+    'banget': 'sekali',
+    'banged': 'sekali',
+    
+    # TAPI
+    'tp'    : 'tapi',
+    'tpi'   : 'tapi',
+    
+    # SEPERTI
+    'spt'   : 'seperti',
+    'sprti' : 'seperti',
+    
+    
+    # JADI
+    'jd'    : 'jadi',
+    'jdi'   : 'jadi',
+    
+    
+    # DENGAN
+    'dgn'   : 'dengan',
+    
+    # GANTI
+    'gnti'  : 'ganti',
+    
+    
+    
+    # SUDAH
+    'sdh'   : 'sudah',
+    'dah'   : 'sudah',
+    'udh'   : 'sudah',
+    'uda'   : 'sudah',
+    'suda'   : 'sudah',
+    'dh'   : 'sudah',
+    
+    # KELUAR
+    'kluar' : 'keluar',
+    
+    
+    # HARUS
+    'hrs'   : 'harus',
+    'hrus'  : 'harus',
+    
+    # HARGA
+    'hrga'  : 'harga',
+    'hrg'   : 'harga',
+    
+    # TERUS
+    'trs'   : 'terus',
+    'trus'  : 'terus',
+    
+    # PESAN
+    'pesen' : 'pesan',
+    'psn'   : 'pesan',
+    'psen'   : 'pesan',
+    'order'   : 'pesan',
+    
+    # JUGA
+    'jg'    : 'juga',
+    'jga'   : 'juga',
+    
+    # ERROR
+    'eror'  : 'error',
+    
+    # CEPAT
+    'cpt'   : 'cepat',
+    'cpat'  : 'cepat',
+    'cept'  : 'cepat',
+    
+    # TOP UP
+    'top up'    : 'topup',
+    'top-up'    : 'topup',
+    'isi saldo' : 'topup',
+    
+    # SEKARANG
+    'skrg'  : 'sekarang',
+    'skrang': 'sekarang',
+    
+    # SALAH
+    'slah'  : 'salah',
+    'slh'   : 'salah',
+    'slah'   : 'salah',
+    'salh'   : 'salah',
+    
+    # MASIH
+    'msih'  : 'masih',
+    'msh'   : 'masih',
+    
+    # MAU
+    'mo'    : 'mau',
+    'mw'    : 'mau',
+    'mau'    : 'mau',
+    
+    # Memasukkan
+    'msukin'    : 'memasukkan',
+    'masukin'   : 'memasukkan',
+    'mskkin'    : 'memasukkan',
+    'msukkin'   : 'memasukkan',
+    
+    # BANYAK
+    'bnyk'  : 'banyak',
+    'byk'   : 'banyak',
+    'bayk'  : 'banyak',
+    
+    # BAYAR
+    'byr'   : 'bayar',
+    'byar'  : 'bayar',
+    
+    # SAMA
+    'sm'    : 'sama',
+    'sma'   : 'sama',
+    
+    # SAMPAI
+    'smpe'  : 'sampai',
+    'sampe'  : 'sampai',
+    'ampe'  : 'sampai',
+    'mpe'  : 'sampai',
+    
+    # APLIKASI
+    'apk'   : 'aplikasi',
+    'apl'   : 'aplikasi',
+    
+    # BELI
+    'bli' : 'beli',
+    
+    # LEBIH
+    'lbih'  : 'lebih',
+    'lbh'  : 'lebih',
+    'lebh'  : 'lebih',
+    
+    # RUMAH
+    'rmh'   : 'rumah',
+    'rmah'  : 'rumah',
+    
+    # JIKA
+    'lw'    : 'jika',
+    'klo'   : 'jika',
+    'klau'  : 'jika',
+    'klu'   : 'jika',
+    'klw'   : 'jika',
+    'kalo'  : 'jika',
+    'kalau' : 'jika',
+    'qlw' : 'jika',
+    
+    # TELEPON
+    'tlp'   : 'telepon',
+    'tlpn'  : 'telepon',
+    'tlpon' : 'telepon',
+    'tlepon': 'telepon',
+    'tlepn': 'telepon',
+    'nlpon': 'telepon',
+    'nlpn': 'telepon',
+    'tlfn': 'telepon',
+    'ditlfn': 'telepon',
+    
+    
+    # DARI
+    'dri'   : 'dari',
+    'dr'    : 'dari',
+    
+    # DRIVER
+    'drever'    : 'driver',
+    'draver'    : 'driver',
+    
+    # DULU
+    'dlu'   : 'dulu',
+    'dl'    : 'dulu',
+    
+    # KARENA
+    'krn'   : 'karena',
+    'karna' : 'karena',
+    'kerna' : 'karena',
+    'krna'  : 'karena',
+    
+    # MENDAPAT
+    'dpt'     : 'mendapatkan',
+    'dapet'   : 'mendapatkan',
+    'dapat'   : 'mendapatkan',
+    
+    # TERNYATA
+    'trnyata' : 'ternyata',
+    'trnyta'  : 'ternyata',
+    
+    # SULIT
+    'susah'   : 'sulit',
+    'slit'    : 'sulit',
+    'ssah'    : 'sulit',
+    'sush'    : 'sulit', 
+    
+    
+    # VOUCHER
+    'vocer'  : 'voucher',
+    'voucer'  : 'voucher',
+    'vocher' : 'voucher',
+    'focher' : 'voucher',
+    'focer' : 'voucher',
+    'kupon'  : 'voucher',
+    'vc'  : 'voucher',
+    'discount' : 'diskon',
+    'disc' : 'diskon',
+    
+    # UPDATE
+    'apdet' : 'update',
+    'updet' : 'update',
+    'updt' : 'update',
+    'apdt' : 'update',
+    'updat' : 'update',
+    'apdat' : 'update',
+    
+    # UNTUK
+    'utk'   : 'untuk',
+    'tuk'   : 'untuk',
+    'untk'  : 'untuk',
+    
+    # BISA
+    'bsa'   : 'bisa',
+    'bs'    : 'bisa',
+    
+    # HABIS
+    'abis'  : 'habis',
+    'hbs'   : 'habis',
+    'hbis'   : 'habis',
+    
+    
+    # EMAIL
+    'e-mail' : 'email',
+    
+    # MAHAL
+    'mhl'   : 'mahal',
+    'mhal'  : 'mahal',
+    'mehong': 'mahal',
+    
+    # UPGRADE
+    'up grade' : 'upgrade',
+    'apgred' : 'upgrade',
+    'up-grade' : 'upgrade',
+    'upgradenya' : 'upgrade',
+    'upgred' : 'upgrade',
+    'upgret' : 'upgrade',
+    'apgret'    : 'upgrade',
+    'apgred'    : 'upgrade',
+    
+    # BULAN
+    'bln'   : 'bulan',
+    'blan'  : 'bulan',
+    'buln'  : 'bulan',
+    
+    # BURAM
+    'burem' : 'buram',
+    
+    
+    # CUSTOMER
+    'cust'  : 'customer',
+    'kastemer'  : 'customer',
+    'kastamer'  : 'customer',
+    'castemer'  : 'customer',
+    'castamer'  : 'customer',
+    'custamer'  : 'customer',
+    'custemer'  : 'customer',
+    'cst'  : 'customer',
+    'castumer'  : 'customer',
+    
+    # BATAL
+    'cancel'    : 'batal',
+    'cansel'    : 'batal',
+    'censel'    : 'batal',
+    'cencle'    : 'batal',
+    'cencel'    : 'batal',
+    'btl'    : 'batal',
+    'btal'    : 'batal',
+    'batl'    : 'batal',
+    'cancle'    : 'batal',
+    
+    # ADMIN
+    'adm'   : 'admin',
+    'admn'  : 'admin',
+    
+    # ATAU
+    'ato' : 'atau',
+    'atw'   : 'atau',
+    
+    # NOMOR
+    'no'   :'nomor',
+    'nomer' : 'nomor',
+    'nmr'   : 'nomor',
+    'nomr'   : 'nomor',
+    
+    # INTERFACE
+    'UI' : 'interface',
+    
+    # DOWNLOAD
+    'donlot'    : 'download',
+    'downlod'   : 'download',
+    
+    # JANGAN
+    'jgn'   : 'jangan',
+    
+    # SISTEM
+    'system'    : 'sistem',
+    'sisteam'    : 'sistem',
+    
+    # ORDERAN FIKTIF
+    'opik'  : 'orderan fiktif',
+    'opic'  : 'orderan fiktif',
+    'opick'  : 'orderan fiktif',
+    
+        
+    # GAPENTING
+    'donk'  : '',
+    'donx'  : '',
+    'dong'  : '',
+    'lah'   : '',
+    'koq'   : '',
+    'kok'   : '',
+    'ko'    : '',
+    'sih'   : '',
+    'si'   : '',
+    'govlok'   : '',
+    'goblok'   : '',
+    'anjing'   : '',
+    'anjir'   : '',
+    'anjay'   : '',
+    'bangsat'   : '',
+    'babi'   : '',
+    'kintil'   : '',
+    
+       
+}
